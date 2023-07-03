589d271f408940fb0fa261e3d10855a6176cab25
id <id@192.168.10.105>
2023-07-03 17:15:19 +0900

    7月3日の成果

--- src/main/java/com/example/demo/controller/MypageController.java
+++ src/main/java/com/example/demo/controller/MypageController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,42 +31,80 @@ public class MypageController {
 	@Autowired
 	UserRepository userRepository;
     
-	//マイページの表示
+	//「マイページ」の表示
     @GetMapping("/mypage/{name}")
-    String showmypage(
-//    		@RequestParam(name="name", required=true)String name,
+    String index(
     		@PathVariable("name")String name,
     		Model model) {
-    	System.out.println(name);
     	Optional<User> userInfo = userRepository.findByName(name);
     	model.addAttribute("userInfo", userInfo.get());
     	
         return "mypage";
     }
     
-//    @GetMapping("/editaccount")
-//    String jumppage(
-//    		
-//    		Model model){
-//    	return "editInfo";
-//    }
-//    
-//    @PostMapping("/editaccount")
-//    String editinfo(
-//    		@RequestParam(name = "name", required = false) String name,
-//    		@RequestParam(name = "address", required = false) String address,
-//    		@RequestParam(name = "tel", required = false) String tel,
-//    		@RequestParam(name = "email", required = false) String email,
-//    		@RequestParam(name = "password", required = false) String password,
-//    		Model model){
-//    	model.addAttribute("name", name);
-//    	model.addAttribute("address", address);
-//    	model.addAttribute("tel", tel);
-//    	model.addAttribute("email", email);
-//    	model.addAttribute("password", password);
-//    	
-//    	return "editInfo";
-//    }
+    //「アカウント情報の編集」画面の表示
+    @GetMapping("/editaccount/{name}")
+    String show(
+    		@PathVariable("name")String name,
+    		Model model){
+    	Optional<User> userInfo = userRepository.findByName(name);
+    	model.addAttribute("userInfo", userInfo.get());
+    	return "editInfo";
+    }
+    
+    //アカウントの編集
+    @PostMapping("/editaccount/{uname}")
+    String update(
+    		@PathVariable("uname")String uname,
+    		@RequestParam(name = "name", required = false) String name,
+			@RequestParam(name = "address", required = false) String address,
+			@RequestParam(name = "tel", required = false) String tel,
+			@RequestParam(name = "email", required = false) String email,
+			@RequestParam(name = "password", required = false) String password,
+    		Model model){
+    	
+    	List<String> messages = new ArrayList<>();
 
+		if (name == null || name.equals("")) {
+			messages.add("名前は必須です");
+		}
+
+		if (address == null || address.equals("")) {
+			messages.add("住所は必須です");
+		}
+
+		if (tel == null || tel.equals("")) {
+			messages.add("電話番号は必須です");
+		}
+
+		if (email == null || email.equals("")) {
+			messages.add("メールアドレスは必須です");
+		} else {
+			Optional<User> registored = userRepository.findByEmail(email);
+
+			if (!registored.isEmpty()) {
+				messages.add("登録済みのメールアドレスです");
+			}
+		}
+
+		if (password == null || password.equals("")) {
+			messages.add("パスワードは必須です");
+		}
+
+		if (messages.size() > 0) {
+			model.addAttribute("messages", messages);
+
+			model.addAttribute("name", name);
+			model.addAttribute("address", address);
+			model.addAttribute("tel", tel);
+			model.addAttribute("email", email);
+			return "editInfo";
+		}
+    	
+		User newUserInfo = new User(name, address, tel, email, password);
+		userRepository.save(newUserInfo);
+
+		return "mypage";
+    }
     
 }
